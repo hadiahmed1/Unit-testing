3a864058a11ce3fdfd6e2a71479141bd8ddd4afe
Hadi <969hadiahmed@gmail.com>
2025-05-19 11:19:36 +0530

    getStringInfo

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,19 @@
-export function toUpper(str: string){
+export function toUpper(str: string) {
     return str.toUpperCase();
+}
+
+export type stringInfo = {
+    lowerCase: string,
+    upperCase: string,
+    character: string[],
+    extraInfo: Object | undefined
+}
+
+export function getStringInfo(str: string): stringInfo {
+    return {
+        lowerCase: str.toLowerCase(),
+        upperCase: str.toUpperCase(),
+        character: Array.from(str),
+        extraInfo: { length: str.length }
+    }
 }
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,30 @@
-import { toUpper } from "../app/Utils"
+import { getStringInfo, stringInfo, toUpper } from "../app/Utils"
 
- describe('Ustils test suite',()=>{
-    test('should return upper case of string',()=>{
-        const result = toUpper('abc');
-        expect(result).toBe('ABC');
+describe('Ustils test suite', () => {
+    it('should return upper case of a valid string', () => {
+        //arrange
+        const sut = toUpper;
+        const expected = 'ABC';
+
+        //act
+        const actual = sut('abc');
+
+        //assert
+        expect(actual).toBe(expected);
+    });
+
+    it('should return string info for valid string', () => {
+        const sut = getStringInfo;
+
+        const actual = sut('My-string');
+
+        expect(actual.lowerCase).toBe('my-string');
+        expect(actual.extraInfo).toEqual({});
+        expect(actual.extraInfo).not.toBe(undefined);
+        expect(actual.character).toHaveLength(9);
+        expect(actual.character).toContain('M');
+        expect(actual.character).toEqual(
+            expect.arrayContaining(['M', 's', 't', 'y', '-', 'r', 'i', 'n', 'g'])
+        );
     })
- })
+})
